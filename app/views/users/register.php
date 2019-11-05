2e69fa15aab3429c9e1b36312b8883892de4bc78
Pratik Solanki <pratiks.solanki3@gmail.com>
2019-11-05 20:33:14 +0000

    Form UI added

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -10,6 +10,21 @@
                         <input type="text" name="name" class="form-control form-control-lg <?php echo (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['name']; ?>">
                         <span class="invalid-feedback"><?php echo $data['name_err'] ?></span>
                     </div>
+                    <div class="form-group">
+                        <label for="email">Email: <sup>*</sup></label>
+                        <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
+                        <span class="invalid-feedback"><?php echo $data['email_err'] ?></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password: <sup>*</sup></label>
+                        <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['password']; ?>">
+                        <span class="invalid-feedback"><?php echo $data['password_err'] ?></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="confirm_password">Confirm Password: <sup>*</sup></label>
+                        <input type="password" name="confirm_password" class="form-control form-control-lg <?php echo (!empty($data['confirm_password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['confirm_password']; ?>">
+                        <span class="invalid-feedback"><?php echo $data['confirm_password_err'] ?></span>
+                    </div>
                 </form>
             </div>
         </div>
