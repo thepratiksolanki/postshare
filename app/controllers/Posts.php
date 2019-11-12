0fa2e132bab05d8f5ca3380846cbf8497f2aec3c
Pratik Solanki <pratiks.solanki3@gmail.com>
2019-11-12 02:03:56 +0000

    Display post and session helper for checking logged in user

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,8 +1,21 @@
 <?php
     class Posts extends Controller {
+
+        public function __construct(){
+            if(!isLoggedIn()){
+                redirect('users/login');
+            }
+
+            $this->postModel = $this->model('Post');
+        }
+
         public function index(){
 
-            $data = [];
+            $posts = $this->postModel->getPosts();
+
+            $data = [
+                'posts' => $posts
+            ];
 
             $this->view('posts/index', $data);
         }
--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -163,12 +163,4 @@
             session_destroy();
             redirect('users/login');
         }
-
-        public function isLoggedIn(){
-            if(isset($_SESSION['user_id'])){
-                return true;
-            } else {
-                return false;
-            }
-        }
     }
